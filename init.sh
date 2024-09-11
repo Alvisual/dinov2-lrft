6c37d5d038bb371385a9fdd564e7aa8946099027
yusong <yusong.leng@gmail.com>
2024-09-11 13:42:42 +0800

    feat: Add script to setup GPU server for DINOv2 dev

---
+++ init.sh
@@ -0,0 +1,180 @@
+# https://medium.com/@milistu/how-to-install-cuda-cudnn-7e4a00ae4f44
+
+ii  libnvidia-cfg1-550-server:amd64      550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA binary OpenGL/GLX configuration library
+ii  libnvidia-compute-550-server:amd64   550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA libcompute package
+ii  libnvidia-container-tools            1.15.0-1                                amd64        NVIDIA container runtime library (command-line tools)
+ii  libnvidia-container1:amd64           1.15.0-1                                amd64        NVIDIA container runtime library
+ii  nvidia-compute-utils-550-server      550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA compute utilities
+ii  nvidia-container-toolkit             1.15.0-1                                amd64        NVIDIA Container toolkit
+ii  nvidia-container-toolkit-base        1.15.0-1                                amd64        NVIDIA Container Toolkit Base
+ii  nvidia-dkms-550-server               550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA DKMS package
+ii  nvidia-fabricmanager-550             550.54.15-0ubuntu0.22.04.1              amd64        Fabric Manager for NVSwitch based systems.
+ii  nvidia-firmware-550-server-550.54.15 550.54.15-0ubuntu0.22.04.2              amd64        Firmware files used by the kernel module
+ii  nvidia-headless-550-server           550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA headless metapackage
+ii  nvidia-headless-no-dkms-550-server   550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA headless metapackage - no DKMS
+ii  nvidia-kernel-common-550-server      550.54.15-0ubuntu0.22.04.2              amd64        Shared files used with the kernel module
+ii  nvidia-kernel-source-550-server      550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA kernel source package
+ii  nvidia-utils-550-server              550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA Server Driver support binaries
+
+ii  libnvidia-cfg1-550-server:amd64      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA binary OpenGL/GLX configuration library
+ii  libnvidia-compute-550-server:amd64   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA libcompute package
+ii  libnvidia-container-tools            1.15.0-1                                amd64        NVIDIA container runtime library (command-line tools)
+ii  libnvidia-container1:amd64           1.15.0-1                                amd64        NVIDIA container runtime library
+ii  nvidia-compute-utils-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA compute utilities
+ii  nvidia-container-toolkit             1.15.0-1                                amd64        NVIDIA Container toolkit
+ii  nvidia-container-toolkit-base        1.15.0-1                                amd64        NVIDIA Container Toolkit Base
+ii  nvidia-dkms-550-server               550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA DKMS package
+ii  nvidia-fabricmanager-550             550.54.15-0ubuntu0.22.04.1              amd64        Fabric Manager for NVSwitch based systems.
+ii  nvidia-firmware-550-server-550.54.15 550.54.15-0ubuntu0.22.04.2              amd64        Firmware files used by the kernel module
+ii  nvidia-firmware-550-server-550.90.07 550.90.07-0ubuntu0.22.04.1              amd64        Firmware files used by the kernel module
+ii  nvidia-headless-550-server           550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA headless metapackage
+ii  nvidia-headless-no-dkms-550-server   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA headless metapackage - no DKMS
+ii  nvidia-kernel-common-550-server      550.90.07-0ubuntu0.22.04.1              amd64        Shared files used with the kernel module
+ii  nvidia-kernel-source-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA kernel source package
+ii  nvidia-utils-550-server              550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA Server Driver support binaries
+
+ii  cudnn-local-repo-ubuntu2204-9.3.0    1.0-1                                   amd64        cudnn-local repository configuration files
+ii  cudnn9-cuda-11                       9.3.0.75-1                              amd64        NVIDIA cuDNN for CUDA 11
+ii  cudnn9-cuda-11-8                     9.3.0.75-1                              amd64        NVIDIA cuDNN for CUDA 11.8
+ii  libcudnn9-cuda-11                    9.3.0.75-1                              amd64        cuDNN runtime libraries for CUDA 11.8
+ii  libcudnn9-dev-cuda-11                9.3.0.75-1                              amd64        cuDNN development headers and symlinks for CUDA 11.8
+ii  libcudnn9-static-cuda-11             9.3.0.75-1                              amd64        cuDNN static libraries for CUDA 11.8
+ii  libnvidia-cfg1-550-server:amd64      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA binary OpenGL/GLX configuration library
+ii  libnvidia-compute-550-server:amd64   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA libcompute package
+ii  libnvidia-container-tools            1.15.0-1                                amd64        NVIDIA container runtime library (command-line tools)
+ii  libnvidia-container1:amd64           1.15.0-1                                amd64        NVIDIA container runtime library
+ii  nvidia-compute-utils-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA compute utilities
+ii  nvidia-container-toolkit             1.15.0-1                                amd64        NVIDIA Container toolkit
+ii  nvidia-container-toolkit-base        1.15.0-1                                amd64        NVIDIA Container Toolkit Base
+ii  nvidia-dkms-550-server               550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA DKMS package
+ii  nvidia-fabricmanager-550             550.54.15-0ubuntu0.22.04.1              amd64        Fabric Manager for NVSwitch based systems.
+ii  nvidia-firmware-550-server-550.54.15 550.54.15-0ubuntu0.22.04.2              amd64        Firmware files used by the kernel module
+ii  nvidia-firmware-550-server-550.90.07 550.90.07-0ubuntu0.22.04.1              amd64        Firmware files used by the kernel module
+ii  nvidia-headless-550-server           550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA headless metapackage
+ii  nvidia-headless-no-dkms-550-server   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA headless metapackage - no DKMS
+ii  nvidia-kernel-common-550-server      550.90.07-0ubuntu0.22.04.1              amd64        Shared files used with the kernel module
+ii  nvidia-kernel-source-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA kernel source package
+ii  nvidia-utils-550-server              550.54.15-0ubuntu0.22.04.2              amd64        NVIDIA Server Driver support binaries
+
+(dino) (base) user@c95bc7bc-0895-4712-ab34-c601d7fd24cd:~$ dpkg -l | grep -E 'nvidia|cuda|cudnn|nccl'
+ii  cudnn-local-repo-ubuntu2204-9.3.0    1.0-1                                   amd64        cudnn-local repository configuration files
+ii  cudnn9-cuda-11                       9.3.0.75-1                              amd64        NVIDIA cuDNN for CUDA 11
+ii  cudnn9-cuda-11-8                     9.3.0.75-1                              amd64        NVIDIA cuDNN for CUDA 11.8
+ii  libcudnn9-cuda-11                    9.3.0.75-1                              amd64        cuDNN runtime libraries for CUDA 11.8
+ii  libcudnn9-dev-cuda-11                9.3.0.75-1                              amd64        cuDNN development headers and symlinks for CUDA 11.8
+ii  libcudnn9-static-cuda-11             9.3.0.75-1                              amd64        cuDNN static libraries for CUDA 11.8
+ii  libnvidia-cfg1-550-server:amd64      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA binary OpenGL/GLX configuration library
+ii  libnvidia-compute-550-server:amd64   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA libcompute package
+ii  libnvidia-container-tools            1.16.1-1                                amd64        NVIDIA container runtime library (command-line tools)
+ii  libnvidia-container1:amd64           1.16.1-1                                amd64        NVIDIA container runtime library
+ii  nvidia-compute-utils-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA compute utilities
+ii  nvidia-container-toolkit             1.16.1-1                                amd64        NVIDIA Container toolkit
+ii  nvidia-container-toolkit-base        1.16.1-1                                amd64        NVIDIA Container Toolkit Base
+ii  nvidia-dkms-550-server               550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA DKMS package
+ii  nvidia-fabricmanager-550             550.90.07-0ubuntu0.22.04.1              amd64        Fabric Manager for NVSwitch based systems.
+ii  nvidia-firmware-550-server-550.90.07 550.90.07-0ubuntu0.22.04.1              amd64        Firmware files used by the kernel module
+ii  nvidia-headless-550-server           550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA headless metapackage
+ii  nvidia-headless-no-dkms-550-server   550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA headless metapackage - no DKMS
+ii  nvidia-kernel-common-550-server      550.90.07-0ubuntu0.22.04.1              amd64        Shared files used with the kernel module
+ii  nvidia-kernel-source-550-server      550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA kernel source package
+ii  nvidia-utils-550-server              550.90.07-0ubuntu0.22.04.1              amd64        NVIDIA Server Driver support binaries
+
+sudo apt-get update
+# sudo apt-get remove --purge -y '*nvidia*' '*cuda*' 'libcudnn*' 'libnccl*' '*cudnn*' '*nccl*'
+# sudo apt-get autoremove --purge -y
+sudo apt-get clean
+dpkg -l | grep -E 'nvidia|cuda|cudnn|nccl'
+
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
+# https://developer.nvidia.com/cuda-11-7-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
+# https://developer.nvidia.com/cuda-11-8-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
+# cd ~ && wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run
+# sudo sh cuda_11.7.0_515.43.04_linux.run
+# echo "export PATH=/usr/local/cuda-11.7/bin${PATH:+:${PATH}}" >> ~/.bashrc
+# echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
+cd ~ && wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sudo sh cuda_11.8.0_520.61.05_linux.run
+echo "export PATH=/usr/local/cuda-11.8/bin${PATH:+:${PATH}}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
+source ~/.bashrc
+
+# nvcc: NVIDIA (R) Cuda compiler driver
+# Copyright (c) 2005-2022 NVIDIA Corporation
+# Built on Wed_Sep_21_10:33:58_PDT_2022
+# Cuda compilation tools, release 11.8, V11.8.89
+# Build cuda_11.8.r11.8/compiler.31833905_0
+
+# https://developer.nvidia.com/cudnn-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
+cd ~ && wget https://developer.download.nvidia.com/compute/cudnn/9.3.0/local_installers/cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+sudo dpkg -i cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+sudo cp /var/cudnn-local-repo-ubuntu2204-9.3.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update -y
+sudo apt-get -y install cudnn-cuda-11
+
+# https://docs.anaconda.com/miniconda/
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+cd ~ && ./miniconda3/bin/conda init bash
+source ~/.bashrc
+
+git config --global user.name "yusong"
+git config --global user.email "yusong.leng@gmail.com"
+
+# https://github.com/git-lfs/git-lfs?tab=readme-ov-file#from-binary
+cd ~ && wget https://github.com/git-lfs/git-lfs/releases/download/v3.5.1/git-lfs-linux-amd64-v3.5.1.tar.gz
+tar -xvzf git-lfs-linux-amd64-v3.5.1.tar.gz
+sudo ./git-lfs-3.5.1/install.sh
+
+cd ~ && git clone https://github.com/Alvisual/datasets-trichotrack.git
+cd datasets-trichotrack && git lfs install
+
+cd ~ && git clone https://github.com/Alvisual/dinov2-lrft.git
+cd dinov2-lrft
+git lfs install
+git remote add root https://github.com/facebookresearch/dinov2.git
+git fetch root
+git checkout -b root root/main
+git switch main
+
+conda create -n dino python=3.11.9
+conda activate dino
+pip install -r requirements.txt
+# numpy==1.26.4
+# --extra-index-url https://download.pytorch.org/whl/cu118
+# torch==2.0.1
+# torchvision==0.15.2
+# omegaconf
+# torchmetrics==0.10.3
+# fvcore
+# iopath
+# # https://github.com/facebookresearch/xformers/blob/main/CHANGELOG.md#0021---2023-08-18
+# xformers==0.0.20
+# submitit
+# wandb
+# pandas
+# opencv-python-headless
+pip cache purge && pip cache info
+
+
+cat <<EOL >> ~/.bashrc
+alias gbranch="git branch -vv"
+alias glog="git log --graph --color --abbrev-commit --pretty=format:'%C(bold red)%h%Creset -%C(auto)%d%Creset %s %C(bold green)(%cs) %C(bold blue)<%an>%Creset'"
+alias gl="git log --pretty=oneline"
+alias gclean="git fetch --prune && git gc"
+alias gdbl="git branch -D"
+alias gdbr="git push origin --delete"
+alias gpnb="git push -u origin"
+alias gdtl="git tag --delete"
+alias gdtr="git push origin --delete"
+alias gpnt="git push origin --tags"
+alias plod="pip list --outdate"
+alias pinstall="pip install -U"
+alias pclean="pip cache purge && pip cache info"
+EOL
+source ~/.bashrc
+
+
+rm cudnn-local-repo-ubuntu2204-9.3.0_1.0-1_amd64.deb
+rm cuda_11.7.0_515.43.04_linux.run
